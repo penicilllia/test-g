98380332714c5a6957c7ba1d2e9f297a19125f55
penicilllia <penicilllia@gmail.com>
2022-02-21 09:30:16 +0500

    Валицации и scope методы

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,15 @@
 class Question < ApplicationRecord
   has_many :answers
   belongs_to :test
+
+  validates :body, presence: true
+
+  validates :validate_answers_count, allow_nil: true
+
+  private
+
+  def validate_answers_count
+    errors.add(:answers) if (answers.count > 4)
+  end
+
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -5,7 +5,35 @@ class Test < ApplicationRecord
   has_many :tests_users
   has_many :users, through: :tests_users
 
-  def self.category_list(category_name)
-    joins(:category).where('categories.title like ?', category_name).order(title: :desc).pluck(:title)
+  #validates :title, presence: true,
+  #                  uniqueness: true
+
+  #validates :level, numericality: { only_integer: true }
+
+  #with_options if: :is_test? do |test|
+  #  test.validates :title, presence: true
+  #  test.validates :title, uniqueness: true
+  #  test.validates :level, numericality: { only_integer: true }
+  #end
+
+  #validates :validate_positive_level
+
+
+  MAXLEVEL = Float::INFINITY
+
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, ->  { where(level: 5..MAXLEVEL) }
+
+  scope :category_list, -> (category_name) { joins(:category).where('categories.title like ?', category_name).order(title: :desc).pluck(:title) }
+
+  private
+
+  def validate_positive_level
+    errors.add(:level) if level.to_i <= 0
   end
+
+  #def self.category_list(category_name)
+  #  joins(:category).where('categories.title like ?', category_name).order(title: :desc).pluck(:title)
+  #end
 end
