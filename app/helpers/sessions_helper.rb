f2645d3858916cdab09f88b9730a5e97f6fce4c3
penicilllia <penicilllia@gmail.com>
2022-04-26 13:10:22 +0500

    Исправление ошибок 2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,8 @@
 module SessionsHelper
 
   def flash_message
-  #  content_tag :p, flash[:alert], class: 'flash alert'
     flash.each do |key, msg|
       content_tag :p, msg, :id => key, :class => 'flash'
-      #content_tag :p, flash[:key], class: 'flash'
     end
 
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,27 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# category1 = Category.create({ title: 'Python' })
-# category2 = Category.create({ title: "Ruby on Rails" })
+category1 = Category.create({ title: 'Python' })
+category2 = Category.create({ title: "Ruby on Rails" })
 
-# test1 = Test.create({ title: "Basic python", level: 1, category_id: category1.id })
-# test2 = Test.create({ title: "Basic ruby and rails", level: 1, category_id: category2.id }) 
+test1 = Test.create({ title: "Basic python", level: 1, category_id: category1.id })
+test2 = Test.create({ title: "Basic ruby and rails", level: 1, category_id: category2.id }) 
 
-# question1 = Question.create({ body: "Под влиянием какого языка был создан Ruby?", test_id: test2.id })
-# question2 = Question.create({ body: "Как называется самый популярный фреймворк на Ruby?", test_id: test2.id })
-# question3 = Question.create({ body: "Как называется стандартная среда  разработки в python? ", test_id: test1.id })
+question1 = Question.create({ body: "Под влиянием какого языка был создан Ruby?", test_id: test2.id })
+question2 = Question.create({ body: "Как называется самый популярный фреймворк на Ruby?", test_id: test2.id })
+question3 = Question.create({ body: "Как называется стандартная среда  разработки в python? ", test_id: test1.id })
 
-# answer11 = Answer.create({ body: "Под влиянием Perl", correct: true, question_id: question1.id })
-# answer12 = Answer.create({ body: "Под влиянием C++", correct: false, question_id: question1.id })
-# answer13 = Answer.create({ body: "Под влиянием Haslell", correct: false, question_id: question1.id })
+answer11 = Answer.create({ body: "Под влиянием Perl", correct: true, question_id: question1.id })
+answer12 = Answer.create({ body: "Под влиянием C++", correct: false, question_id: question1.id })
+answer13 = Answer.create({ body: "Под влиянием Haslell", correct: false, question_id: question1.id })
 
-# answer21 = Answer.create({ body: "Sinatra", correct: false, question_id: question2.id })
-# answer22 = Answer.create({ body: "Hanami", correct: false, question_id: question2.id })
-# answer23 = Answer.create({ body: "Ruby on Rails", correct: true, question_id: question2.id })
+answer21 = Answer.create({ body: "Sinatra", correct: false, question_id: question2.id })
+answer22 = Answer.create({ body: "Hanami", correct: false, question_id: question2.id })
+answer23 = Answer.create({ body: "Ruby on Rails", correct: true, question_id: question2.id })
 
-# answer31 = Answer.create({ body: "Блокнот", correct: false, question_id: question3.id })
-# answer32 = Answer.create({ body: "IDLE", correct: true, question_id: question3.id })
-# answer33 = Answer.create({ body: "IDOL", correct: false, question_id: question3.id })
-
-user1 = User.create({ name: "Иван Петров", email: "ivan@example.com" })
-user2 = User.create({ name: "Светлана Кузнецова", email: "sveta@example.com" })
+answer31 = Answer.create({ body: "Блокнот", correct: false, question_id: question3.id })
+answer32 = Answer.create({ body: "IDLE", correct: true, question_id: question3.id })
+answer33 = Answer.create({ body: "IDOL", correct: false, question_id: question3.id })
